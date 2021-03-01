b5c46e0b41806570fb857e4f9b61d42d9eb5ca97
Qianna <zhq9669@gmail.com>
2021-03-01 20:14:43 +0800

    modified p of RandomCamStyle

--- configs/mmcl/mmcl_market1501_4gpus.py
+++ configs/mmcl/mmcl_market1501_4gpus.py
@@ -20,7 +20,7 @@ train_pipeline = [
     dict(
         type='RandomCamStyle',
         camstyle_root='bounding_box_train_camstyle',
-        p=0.5),
+        p=0.15),
     dict(
         type='RandomResizedCropV2',
         size=(256, 128),
